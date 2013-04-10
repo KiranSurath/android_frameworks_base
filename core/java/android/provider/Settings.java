a94601a5e3c57e5c309a0fa3b27ed5f8f46209e1
Owain van Brakel <owain.vanbrakel@gmail.com>
2013-04-10 15:45:13 +0200

    fixed
    Change-Id: I1ee33d4f6ee0d1a9fbd23ccc8b83250ef8fb8d57

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6173,12 +6173,12 @@ public final class Settings {
         public static final String POWER_SOUNDS_ENABLED = "power_sounds_enabled";
 
         /**
-<<<<<<< HEAD
          * Whether to allow killing of the foreground app by long-pressing the Back button
          * @hide
          */
         public static final String KILL_APP_LONGPRESS_BACK = "kill_app_longpress_back";
-=======
+
+        /**
          * Whether to sound when charger power is connected/disconnected
          * @hide
          */
@@ -6189,7 +6189,6 @@ public final class Settings {
          * @hide
          */
         public static final String POWER_NOTIFICATIONS_RINGTONE = "power_notifications_ringtone";
->>>>>>> d936133... [1/2] Power connect/disconnect notification support
 
         /**
          * URI for the "wireless charging started" sound.
