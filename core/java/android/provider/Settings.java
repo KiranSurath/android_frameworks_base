2ce5e427ed4d81187d6d1dda38edf731ab60cd9f
Owain van Brakel <owain.vanbrakel@gmail.com>
2013-06-02 03:52:09 +0200

    fix
    Change-Id: I558714beded912d3f4a7856444e18f85198a0637

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2707,12 +2707,6 @@ public final class Settings {
         public static final String QS_DISABLE_PANEL = "qs_disable_panel";
 
         /**
-<<<<<<< HEAD
-         * Use the Notification Power Widget? (Who wouldn't!)
-         *
-=======
-         * Pie size fraction, default is 1.0f (normal)
->>>>>>> 83b26e9... Pie: updates (2/2)
          * @hide
          */
         public static final String EXPANDED_VIEW_WIDGET = "expanded_view_widget";
@@ -3053,7 +3047,7 @@ public final class Settings {
         public static final String PIE_MODE = "pie_mode";
 
         /**
-         * Pie size fraction
+         * Pie size fraction, default is 1.0f (normal)
          * @hide
          */
         public static final String PIE_SIZE = "pie_size";
