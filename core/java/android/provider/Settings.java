0423f55488bf9bd912c13a2134f3108be51afe67
Schubi <schubi@erlangen.ccc.de>
2013-06-06 01:29:12 +0200

    Configurable Notification Behavior [2/2]
    Credits:
    - Michele Primavera
    - mnm9994u (SlimRoms)
    
    Change-Id: Ibb6bd69d4df0bdd664a88213aaab0385ac33a139

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4639,6 +4639,12 @@ public final class Settings {
         public static final String VOLUME_KEYS_CONTROL_RING_STREAM = "volume_keys_control_ring_stream";
 
         /**
+         * Notifications Expand Behavior
+         * @hide
+         */
+        public static final String NOTIFICATIONS_BEHAVIOUR = "notifications_behaviour"; 
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
