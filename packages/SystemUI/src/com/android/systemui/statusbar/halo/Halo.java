5a1114efa5ab6669870066beab9c6291c5e7cf55
Carlo Savignano <stevewatersy@gmail.com>
2013-07-17 12:52:41 +0200

    Halo: Avoid showing Halo))) icons in status bar while added to Halo (Credits @Anu6is)

--- packages/SystemUI/src/com/android/systemui/statusbar/halo/Halo.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/halo/Halo.java
@@ -478,6 +478,7 @@ public class Halo extends FrameLayout implements Ticker.TickerCallback, TabletTi
         final float originalAlpha = mContext.getResources().getFraction(R.dimen.status_bar_icon_drawing_alpha, 1, 1);
         for (int i = 0; i < mNotificationData.size(); i++) {
             NotificationData.Entry entry = mNotificationData.get(i);
+            if (entry.notification.pkg.equals("com.paranoid.halo")) continue;
             entry.icon.setAlpha(originalAlpha);
         }
     }
@@ -486,6 +487,7 @@ public class Halo extends FrameLayout implements Ticker.TickerCallback, TabletTi
         float originalAlpha = mContext.getResources().getFraction(R.dimen.status_bar_icon_drawing_alpha, 1, 1);
         for (int i = 0; i < mNotificationData.size(); i++) {
             NotificationData.Entry entry = mNotificationData.get(i);
+            if (entry.notification.pkg.equals("com.paranoid.halo")) continue;
             entry.icon.setAlpha(index == i ? 1f : originalAlpha);
         }
     }
