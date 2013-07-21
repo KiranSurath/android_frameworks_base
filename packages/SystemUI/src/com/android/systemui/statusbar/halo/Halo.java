a513ac124d1c5532616296cb1f35865f16418981
Owain van Brakel <owain.vanbrakel@gmail.com>
2013-07-21 04:20:59 +0200

    Halo fix
    Change-Id: Ie71e654aa3cf060104099573972e317b30320a60

--- packages/SystemUI/src/com/android/systemui/statusbar/halo/Halo.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/halo/Halo.java
@@ -803,7 +803,7 @@ public class Halo extends FrameLayout implements Ticker.TickerCallback, TabletTi
                             mEffect.setHaloOverlay(HaloProperties.Overlay.NONE, 0f);
                             gestureText = "";
                         } else if (deltaY > 0) {
-                            if (deltaIndex <= 1 && mGesture != Gesture.UP1) {
+                            if (deltaIndex == 1 && mGesture != Gesture.UP1) {
                                 mGesture = Gesture.UP1;
                                 gestureChanged = true;
                                 mEffect.setHaloOverlay(HaloProperties.Overlay.DISMISS, 1f);
@@ -815,13 +815,13 @@ public class Halo extends FrameLayout implements Ticker.TickerCallback, TabletTi
                                 gestureText = mContext.getResources().getString(R.string.halo_clear_all);
                             }
                         } else {
-                            if (deltaIndex <= 1 && mGesture != Gesture.DOWN1) {
+                            if (deltaIndex == 1 && mGesture != Gesture.DOWN1) {
                                 mGesture = Gesture.DOWN1;
                                 gestureChanged = true;
                                 mEffect.setHaloOverlay(mTickerLeft ? HaloProperties.Overlay.BACK_LEFT
                                         : HaloProperties.Overlay.BACK_RIGHT, 1f);
                                 gestureText = mContext.getResources().getString(R.string.halo_hide);
-                            } else if (deltaIndex > 0 && mGesture != Gesture.DOWN2) {
+                            } else if (deltaIndex > 1 && mGesture != Gesture.DOWN2) {
                                 mGesture = Gesture.DOWN2;
                                 gestureChanged = true;
                                 mEffect.setHaloOverlay(mTickerLeft ? HaloProperties.Overlay.SILENCE_LEFT
@@ -829,6 +829,7 @@ public class Halo extends FrameLayout implements Ticker.TickerCallback, TabletTi
                                 gestureText = mContext.getResources().getString(R.string.halo_silence);
                             }
                         }
+
                         if (gestureChanged) {
                             mMarkerIndex = -1;
 
